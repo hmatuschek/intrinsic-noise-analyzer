0c0097afd0a98d390828be3ed56f2fad0caa506f
Philipp <pthomas@sce-bio-c02936.bio.ed.ac.uk>
2012-06-14 16:33:43 +0100

    converter

---
+++ lib/ast/converter.cc
@@ -0,0 +1,105 @@
+#include "converter.hh"
+
+using namespace Fluc;
+using namespace Fluc::Ast;
+
+Convert2Irreversible::Convert2Irreversible(Ast::Model &model)
+    : model(model)
+
+{
+
+}
+
+
+void Convert2Irreversible::process()
+{
+  size_t count=0;
+  // Iterate over all reactions:
+  for (Ast::Model::iterator iter = model.begin(); iter != model.end(); iter++)
+  {
+    // Skip all non-reaction definitions:
+    if (! Ast::Node::isReactionDefinition(*iter))
+      continue;
+
+    Ast::Reaction *reaction= static_cast<Ast::Reaction *>(*iter);
+
+    // Check if reaction is reversible:
+    if (reaction->isReversible())
+    {
+
+      GiNaC::ex numerator = reaction->getKineticLaw()->getRateLaw().numer().expand();
+      GiNaC::ex denominator = reaction->getKineticLaw()->getRateLaw().denom();
+
+      GiNaC::ex forwardLaw = numerator;
+      GiNaC::ex backwardLaw = numerator;
+
+      for(size_t i=0; i<model.numSpecies(); i++)
+      {
+
+          if ( reaction->hasReactant( model.getSpecies(i)->getSymbol()) )
+          {
+              std::cerr<<i<<std::endl;
+              backwardLaw=backwardLaw.subs(model.getSpecies(i)->getSymbol()==0);
+          }
+          if ( reaction->hasProduct(model.getSpecies(i)->getSymbol()) )
+          {
+              forwardLaw=forwardLaw.subs(model.getSpecies(i)->getSymbol()==0);
+          }
+      }
+
+      if (!numerator.is_equal(forwardLaw+backwardLaw))
+      {
+          SBMLFeatureNotSupported err;
+          err << "Reversible reaction "
+              << (*iter)->getIdentifier() << " could not be converted to a reversible one!";
+          throw err;
+      }
+
+      forwardLaw=forwardLaw/denominator;
+      backwardLaw=-backwardLaw/denominator;
+
+
+      std::cerr<<reaction->getName()<<std::endl;
+
+      std::cerr<<"numerator: "<<numerator<<std::endl;
+      std::cerr<<"forward: "<<forwardLaw<<std::endl;
+      std::cerr<<"backward: "<<backwardLaw<<std::endl;
+
+      // set forward rate law
+      reaction->setReversible(false);
+      reaction->getKineticLaw()->setRateLaw(forwardLaw);
+
+      // create a new irreversible backward reaction
+      std::string id = reaction->getIdentifier();
+      std::string name = reaction->getName();
+
+      id=id.append("Reverse");
+      name.append(" (Reverse)");
+      Ast::KineticLaw *kineticLaw = new Ast::KineticLaw(backwardLaw);
+      Ast::Reaction *backwardReaction = new Ast::Reaction(id, name, kineticLaw,false);
+
+      // swap reactant and products in reverse reaction
+      for(Ast::Reaction::iterator species = reaction->reacBegin(); species!=reaction->reacEnd(); species++)
+      {
+              GiNaC::ex st = (reaction->getReactantStoichiometry(species->first->getSymbol()));
+              backwardReaction->setProductStoichiometry( species->first,st );
+      }
+      for(Ast::Reaction::iterator species = reaction->prodBegin(); species!=reaction->prodEnd(); species++)
+      {
+              GiNaC::ex st = ( reaction->getProductStoichiometry(species->first->getSymbol()) );
+              backwardReaction->setReactantStoichiometry( species->first,st );
+      }
+
+      // add backward reaction
+      model.addDefinition(backwardReaction);
+
+      count++;
+
+      std::cerr<<"success"<<std::endl;
+
+    }
+  }
+
+  if(count) std::cerr<<"Converted "<<count<<" reactions succesfully."<<std::endl;
+
+}
---
+++ lib/ast/converter.hh
@@ -0,0 +1,34 @@
+#ifndef __FLUC_MODELS_CONVERTTOIRREVERSIBLEREACTIONS_HH__
+#define __FLUC_MODELS_CONVERTTOIRREVERSIBLEREACTIONS_HH__
+
+#include "ast/ast.hh"
+
+namespace Fluc {
+namespace Ast {
+
+
+/**
+ * Checks if the model is not defined as reversible.
+ *
+ * @ingroup models
+ */
+class Convert2Irreversible
+{
+
+ Model &model;
+
+public:
+  /**
+   * Constructor, converts to irreversible reactions.
+   */
+  Convert2Irreversible(Ast::Model &model);
+
+  void process();
+
+};
+
+
+}
+}
+
+#endif // ASSERTIRREVERSIBLEREACTIONS_HH
