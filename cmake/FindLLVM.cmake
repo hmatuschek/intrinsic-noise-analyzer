f8f9e1169610a215a67aaca8920dad3a361d330f
Hannes Matuschek <hmatuschek@gmail.com>
2012-06-18 16:29:41 +0200

    Fixed build script for LLVM 3.1...

--- cmake/FindLLVM.cmake
+++ cmake/FindLLVM.cmake
@@ -1,4 +1,5 @@
 SET(LLVM_CONFIG_EXEC_NAMES "llvm-config"
+    "llvm-config-mp-3.1" "llvm-config-3.1"
     "llvm-config-mp-3.0" "llvm-config-3.0"
     "llvm-config-mp-2.9" "llvm-config-2.9")
 FIND_PROGRAM(LLVM_CONFIG_EXEC NAMES ${LLVM_CONFIG_EXEC_NAMES})
