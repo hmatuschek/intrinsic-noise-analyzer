57a7599403f0970a326ec02846b7958093136b25
Hannes Matuschek <hmatuschek@gmail.com>
2012-08-09 12:41:52 +0200

    Cleanup

--- lib/trafo/conservationanalysis.cc
+++ lib/trafo/conservationanalysis.cc
@@ -55,14 +55,13 @@ ConservationAnalysis::ConservationAnalysis(Ast::Model &model)
   Eigen::FullPivLU< Eigen::MatrixXd > LU;
   LU.compute(stoichiometry.transpose());
 
+  // Assemble list of dependent and independent species as well as the link matrix:
   size_t n_dep  = LU.dimensionOfKernel();
   size_t n_indep = numSpecies - n_dep;
   Eigen::PermutationMatrix<Eigen::Dynamic> pM(LU.permutationQ().transpose());
-
   Eigen::VectorXex ind_spec = (pM * species).head(n_indep);
   Eigen::VectorXex dep_spec = (pM * species).tail(n_dep);
   Eigen::MatrixXd linkMatrix = -(pM*LU.kernel()).transpose();
-  Eigen::VectorXex link(n_dep);
 
   // Assemble expressions for assignment rules
   // for some reason, the scalar product of double-matrix and expression vector does not work:
@@ -70,11 +69,12 @@ ConservationAnalysis::ConservationAnalysis(Ast::Model &model)
     Ast::Species *species_obj = model.getSpecies(GiNaC::ex_to<GiNaC::symbol>(species(i)));
     GiNaC::ex link = 0;
 
+    // product \vec{link} = linkMatrix * \vec{ind_species}:
     for (size_t j=0; j<n_indep; j++) {
       link += linkMatrix(i,j)*ind_spec(j);
     }
 
-    // Store assignment list list
+    // Store assignment list
     _conserved.append(dep_spec(i) == link);
     // Add assignment rule to dependent species:
     species_obj->setRule(new Ast::AssignmentRule(link));
