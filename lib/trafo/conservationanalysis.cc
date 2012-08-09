b78c7fe700a1d6836ff5eee30c52c61b73a037d8
Hannes Matuschek <hmatuschek@gmail.com>
2012-08-09 12:38:38 +0200

    Implemented conservation analysis that can deal with assignment rules.

---
+++ lib/trafo/conservationanalysis.cc
@@ -0,0 +1,82 @@
+#include "conservationanalysis.hh"
+#include "exception.hh"
+#include "ginacsupportforeigen.hh"
+
+
+using namespace Fluc;
+using namespace Fluc::Trafo;
+
+
+ConservationAnalysis::ConservationAnalysis(Ast::Model &model)
+  : _conserved()
+{
+  // Assemble vector of initially independent species:
+  Eigen::VectorXex species(model.numSpecies());
+  size_t numSpecies=0;
+  for (size_t i=0; i<model.numSpecies(); i++) {
+    if (! model.getSpecies(i)->hasRule()) {
+      species(numSpecies) = model.getSpecies(i)->getSymbol(); numSpecies++;
+    }
+  }
+  species = species.head(numSpecies);
+
+  // Assemble reduced stoichiometry:
+  Eigen::MatrixXd stoichiometry(numSpecies, model.numReactions());
+  for (size_t i=0; i<model.numReactions(); i++) {
+    Ast::Reaction *reaction = model.getReaction(i);
+    for (size_t j=0; j<numSpecies; j++) {
+      if (reaction->hasProduct(GiNaC::ex_to<GiNaC::symbol>(species(j)))) {
+        GiNaC::ex coeff = reaction->getProductStoichiometry(GiNaC::ex_to<GiNaC::symbol>(species(j)));
+        if (! GiNaC::is_a<GiNaC::numeric>(coeff.evalf())) {
+          SBMLFeatureNotSupported err;
+          err << "Can not perform conservation analysis: Stoichiometry of product "
+              << species(j) << " is not a numeric constant.";
+          throw err;
+        }
+        stoichiometry(i,j) = GiNaC::ex_to<GiNaC::numeric>(coeff).to_double();
+      } else {
+        stoichiometry(i,j) = 0;
+      }
+
+      if (reaction->hasReactant(GiNaC::ex_to<GiNaC::symbol>(species(j)))) {
+        GiNaC::ex coeff = reaction->getReactantStoichiometry(GiNaC::ex_to<GiNaC::symbol>(species(j)));
+        if (! GiNaC::is_a<GiNaC::numeric>(coeff.evalf())) {
+          SBMLFeatureNotSupported err;
+          err << "Can not perform conservation analysis: Stoichiometry of reactant "
+              << species(j) << " is not a numeric constant.";
+          throw err;
+        }
+        stoichiometry(i,j) = -GiNaC::ex_to<GiNaC::numeric>(coeff).to_double();
+      }
+    }
+  }
+
+  // Perfrom conservation analysis:
+  Eigen::FullPivLU< Eigen::MatrixXd > LU;
+  LU.compute(stoichiometry.transpose());
+
+  size_t n_dep  = LU.dimensionOfKernel();
+  size_t n_indep = numSpecies - n_dep;
+  Eigen::PermutationMatrix<Eigen::Dynamic> pM(LU.permutationQ().transpose());
+
+  Eigen::VectorXex ind_spec = (pM * species).head(n_indep);
+  Eigen::VectorXex dep_spec = (pM * species).tail(n_dep);
+  Eigen::MatrixXd linkMatrix = -(pM*LU.kernel()).transpose();
+  Eigen::VectorXex link(n_dep);
+
+  // Assemble expressions for assignment rules
+  // for some reason, the scalar product of double-matrix and expression vector does not work:
+  for (size_t i=0; i<n_dep; i++) {
+    Ast::Species *species_obj = model.getSpecies(GiNaC::ex_to<GiNaC::symbol>(species(i)));
+    GiNaC::ex link = 0;
+
+    for (size_t j=0; j<n_indep; j++) {
+      link += linkMatrix(i,j)*ind_spec(j);
+    }
+
+    // Store assignment list list
+    _conserved.append(dep_spec(i) == link);
+    // Add assignment rule to dependent species:
+    species_obj->setRule(new Ast::AssignmentRule(link));
+  }
+}
---
+++ lib/trafo/conservationanalysis.hh
@@ -0,0 +1,32 @@
+#ifndef __INA_TRAFO_CONSERVATIONANALYSIS_HH__
+#define __INA_TRAFO_CONSERVATIONANALYSIS_HH__
+
+#include "ast/ast.hh"
+
+namespace Fluc {
+namespace Trafo {
+
+
+/** This class performs a conservation analysis on the independent species (species having no
+ * rate or assignment rule). Note that all @c Ast::AlgebraicConstraint must be solved and
+ * transformed into @c Ast::AssignmentRule before performing this analysis
+ * (@c Ast::AlgebraicConstraintSolver).
+ * @ingroup trafo */
+class ConservationAnalysis
+{
+protected:
+  /** After conservation analysis, this list contains the assignmens of the conserved cycles as
+   * DEPENDEN_SPECIES == LINEAR_COMBINATION_OF_INDEPENDEND_SPECIES. */
+  GiNaC::lst _conserved;
+
+public:
+  /** Performs the conservation analysis and assembles the conserved cycles, but does not define
+   * any new @c Ast::AssignmentRule for dependent species. */
+  ConservationAnalysis(Ast::Model &model);
+};
+
+
+}
+}
+
+#endif // __INA_TRAFO_CONSERVATIONANALYSIS_HH__
