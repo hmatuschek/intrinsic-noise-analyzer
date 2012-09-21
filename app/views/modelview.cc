873b0abceb353d3f41f2856502232d6a6e2d61c0
Hannes Matuschek <hmatuschek@gmail.com>
2012-09-21 10:55:22 +0200

    Implemented global unit editing.

--- app/views/modelview.cc
+++ app/views/modelview.cc
@@ -23,15 +23,22 @@ ModelView::ModelView(ModelItem *model_item, QWidget *parent)
   label->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
   label->setAlignment(Qt::AlignRight);
 
+  // Editors:
+  _substance_unit_editor = new UnitEditor(_model->getSubstanceUnit());
+  _volume_unit_editor    = new UnitEditor(_model->getLengthUnit());
+  _area_unit_editor      = new UnitEditor(_model->getAreaUnit());
+  _length_unit_editor    = new UnitEditor(_model->getLengthUnit());
+  _time_unit_editor      = new UnitEditor(_model->getTimeUnit());
+
   // Unit box
   QGroupBox *unit_frame = new QGroupBox(tr("Units"));
   unit_frame->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
   QFormLayout *unit_layout = new QFormLayout();
-  unit_layout->addRow(tr("Substance unit"), new UnitEditor(_model->getSubstanceUnit()));
-  unit_layout->addRow(tr("Volume unit"), new UnitEditor(_model->getVolumeUnit()));
-  unit_layout->addRow(tr("Area unit"), new UnitEditor(_model->getAreaUnit()));
-  unit_layout->addRow(tr("Length unit"), new UnitEditor(_model->getLengthUnit()));
-  unit_layout->addRow(tr("Time unit"), new UnitEditor(_model->getTimeUnit()));
+  unit_layout->addRow(tr("Substance unit"), _substance_unit_editor);
+  unit_layout->addRow(tr("Volume unit"), _volume_unit_editor);
+  unit_layout->addRow(tr("Area unit"), _area_unit_editor);
+  unit_layout->addRow(tr("Length unit"), _length_unit_editor);
+  unit_layout->addRow(tr("Time unit"), _time_unit_editor);
   unit_frame->setLayout(unit_layout);
 
   // Layout
@@ -42,4 +49,40 @@ ModelView::ModelView(ModelItem *model_item, QWidget *parent)
 
   // Connect to "desroyed" signal of model_item to close view.
   QObject::connect(model_item, SIGNAL(destroyed()), this, SLOT(deleteLater()));
+  QObject::connect(
+        _substance_unit_editor, SIGNAL(unitChanged()), this, SLOT(onSubstanceUnitChanged()));
+  QObject::connect(
+        _volume_unit_editor, SIGNAL(unitChanged()), this, SLOT(onVolumeUnitChanged()));
+  QObject::connect(
+        _area_unit_editor, SIGNAL(unitChanged()), this, SLOT(onAreaUnitChanged()));
+  QObject::connect(
+        _length_unit_editor, SIGNAL(unitChanged()), this, SLOT(onLengthUnitChanged()));
+  QObject::connect(
+        _time_unit_editor, SIGNAL(unitChanged()), this, SLOT(onTimeUnitChanged()));
+}
+
+
+void
+ModelView::onSubstanceUnitChanged() {
+  _model->setSubstanceUnit(_substance_unit_editor->unit(), true);
+}
+
+void
+ModelView::onVolumeUnitChanged() {
+  _model->setVolumeUnit(_volume_unit_editor->unit(), true);
+}
+
+void
+ModelView::onAreaUnitChanged() {
+  _model->setAreaUnit(_area_unit_editor->unit(), true);
+}
+
+void
+ModelView::onLengthUnitChanged() {
+  _model->setLengthUnit(_length_unit_editor->unit(), true);
+}
+
+void
+ModelView::onTimeUnitChanged() {
+  _model->setTimeUnit(_time_unit_editor->unit(), true);
 }
--- app/views/modelview.hh
+++ app/views/modelview.hh
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include "../doctree/modelitem.hh"
+#include "uniteditor.hh"
 
 
 /** Displays a short summary of a model. */
@@ -14,9 +15,32 @@ public:
   /** Constructor. */
   explicit ModelView(ModelItem *model_item, QWidget *parent = 0);
 
+private slots:
+  /** Callback to update substance unit of model. */
+  void onSubstanceUnitChanged();
+  /** Callback to update volume unit of model. */
+  void onVolumeUnitChanged();
+  /** Callback to update area unit of model. */
+  void onAreaUnitChanged();
+  /** Callback to update length unit of model. */
+  void onLengthUnitChanged();
+  /** Callback to update time unit of model. */
+  void onTimeUnitChanged();
+
 private:
   /** Holds a weak reference to the model instance. */
   iNA::Ast::Model *_model;
+
+  /** Holds the unit editor for the substance unit. */
+  UnitEditor *_substance_unit_editor;
+  /** Holds the unit editor for the volume unit. */
+  UnitEditor *_volume_unit_editor;
+  /** Holds the unit editor for the area unit. */
+  UnitEditor *_area_unit_editor;
+  /** Holds the unit editor for the length unit. */
+  UnitEditor *_length_unit_editor;
+  /** Holds the unit editor for the time unit. */
+  UnitEditor *_time_unit_editor;
 };
 
 #endif // MODELVIEW_HH
