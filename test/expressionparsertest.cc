0e07ff0ae72bb76614d98a3c5bdf971cb384c083
Hannes Matuschek <hmatuschek@gmail.com>
2012-10-17 12:50:34 +0200

    Fixed expression parser & serialization test.

--- test/expressionparsertest.cc
+++ test/expressionparsertest.cc
@@ -14,4 +14,24 @@ ExpressionParserTest::testParser() {
   std::stringstream buffer;
   buffer << "(a-1)*(-c)*(a+b)^-1";
 
+  // Parse expression;
+  GiNaC::ex expression_1 = Parser::Expr::parseExpression(buffer.str(), ctx);
+  // Serialize expression back into string:
+  buffer.str(); Parser::Expr::serializeExpression(expression_1, buffer, ctx);
+  // And parse serialized expression again:
+  GiNaC::ex expression_2 = Parser::Expr::parseExpression(buffer.str(), ctx);
+
+  UT_ASSERT_EQUAL(expression_1, expression_2);
+}
+
+
+UnitTest::TestSuite *
+ExpressionParserTest::suite()
+{
+  UnitTest::TestSuite *s = new UnitTest::TestSuite("Tests for expresion parser.");
+
+  s->addTest(new UnitTest::TestCaller<ExpressionParserTest>(
+               "simple parser test", &ExpressionParserTest::testParser));
+
+  return s;
 }
