cea7871d8feb53af0b8ba0b959da9a73768d93aa
pthomas <pthomas@localhost.localdomain>
2013-03-15 19:20:22 +0000

    clean up

--- cli/ina_cli_ssa.hh
+++
@@ -1,8 +0,0 @@
-#ifndef INA_CLI_SSA_HH
-#define INA_CLI_SSA_HH
-#include <utils/option_parser.hh>
-
-int performSSATimecourseAnalysis(iNA::Utils::Opt::Parser &option_parser);
-
-
-#endif // INA_CLI_SSA_HH
--- cli/sbml_dist.hh
+++
@@ -1,9 +0,0 @@
-#ifndef __CLI_SBML_SSA_HH__
-#define __CLI_SBML_SSA_HH__
-
-#include <iostream>
-#include "ina.hh"
-#include <eigen3/Eigen/Eigen>
-#include <ginac/ginac.h>
-
-#endif
