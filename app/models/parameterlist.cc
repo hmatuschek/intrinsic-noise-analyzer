0ec346985c513bcf76bcef808a2fbcade85edbc5
Hannes Matuschek <hmatuschek@gmail.com>
2014-02-07 12:22:48 +0100

    Fixed include.

--- app/models/parameterlist.cc
+++ app/models/parameterlist.cc
@@ -7,7 +7,7 @@
 #include "../tinytex/ginac2formula.hh"
 #include "../views/unitrenderer.hh"
 #include "referencecounter.hh"
-#include "libina/ast/identifier.hh"
+#include "ast/identifier.hh"
 #include <QMessageBox>
 
 
